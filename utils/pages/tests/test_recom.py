9b74ef15b46db4d6e63b51eb9903b0398871c5ea
Dave Beck <dacb@uw.edu>
2023-12-05 12:40:16 -0800

    added example frameowrk for testing

---
+++ utils/pages/tests/test_recom.py
@@ -0,0 +1,10 @@
+"""Test the recommender system"""
+import unittest
+
+import pandas as pd
+
+class TestReccommend(unittest.TestCase):
+
+    def test_recom_smoke(self):
+        assert(True) # do something useful here
+
